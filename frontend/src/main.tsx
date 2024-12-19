c2dd5256eac682ae5ae9887b87d6c5b65bdf6996
cloudchamb3r <jizon0123@protonmail.com>
2024-12-20 00:03:52 +0900

    Add Configurations for DB, Frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from "@/pages/home.tsx";
+import {World} from "@/pages/world.tsx";
+import {NotFound} from "@/pages/not-found.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<Home/>}></Route>
+            <Route path="/world/:worldId" element={<World/>}></Route>
+            <Route path="*" element={<NotFound/>}/>
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ frontend/src/pages/world.tsx
@@ -0,0 +1,7 @@
+export function World() {
+    return (
+        <>
+            this is world ;)
+        </>
+    )
+}
